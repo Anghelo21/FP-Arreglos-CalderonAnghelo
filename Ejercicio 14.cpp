21c143fdb9d8f7840a60c0e5ca584f0037a4801a
AngheloCM <angheloprog@gmail.com>
2025-06-23 02:52:58 -0500

    Corrección del Ejercicio 14 – Agregar arreglo 2 al arreglo 1 sin String.h, añadiendo función mistrcat, versión final

--- Ejercicio 14.cpp
+++ Ejercicio 14.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void mistrcat(char str1[], char str2[]){
+    int i=0;
+    while(str1[i]!='\0'){
+        i++;
+    }
+    int j=0;
+    while(str2[j] != '\0'){
+        str1[i]=str2[j];
+        i++;
+        j++;
+    }
+    str1[i]='\0';
+}
+
 int main(){
     char str1[100]="Este es ";
     char str2[100]="el ejercicio 14";
-    int i=0;
-    while(str1[i]!='\0'){
-        i=i+1;
-    }
-    int j=0;
-    while(str2[j]!='\0'){
-        str1[i]=str2[j];
-        i=i+1;
-        j=j+1;
-    }
-    str1[i]='\0';
+    mistrcat(str1, str2);
     cout<<"Los arreglos unidos: "<<str1<<endl;
     return 0;
 }
