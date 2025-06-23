aed32371d80eae8a8fb982c7d8d5b0a18dacf8fa
AngheloCM <angheloprog@gmail.com>
2025-06-23 02:41:53 -0500

    Corrección del Ejercicio 13 – Copiar arreglo sin String.h, añadiendo función mistrcpy, versión final

--- Ejercicio 13.cpp
+++ Ejercicio 13.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void mistrcpy(char str1[], char str2[]){
+    int i=0;
+    while(str2[i]!='\0'){
+        str1[i]=str2[i];
+        i++;
+    }
+    str1[i] = '\0';
+}
+
 int main(){
 	char str1[100];
 	char str2[100]="Ejemplo del problema 13";
-	int i=0;
-	while(str2[i]!='\0'){
-		str1[i]=str2[i];
-		i=i+1;
-	}
+	mistrcpy(str1, str2);
 	cout<<"Str1 despues de copiar str2: "<<str1<<endl;
 	return 0;
 }
